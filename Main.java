f13d4533c5848e0a765762a9f37842ab5e17ffed
antoniochen58 <43198397+antoniochen58@users.noreply.github.com>
2018-09-25 18:34:58 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+import java.util.StringJoiner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //攝氏轉華氏
+//        System.out.println("輸入攝氏溫度");
+//        Scanner scn=new Scanner(System.in);
+//        float c= scn.nextFloat();
+//        float f=c*9/5+32;
+//        System.out.println(f);
+
+
+        //合,差及乘積
+//        System.out.println("輸入兩個整數");
+//        Scanner scn=new Scanner(System.in);
+//        int v1=scn.nextInt();
+//        int v2=scn.nextInt();
+//        System.out.println(v1+v2);
+//        System.out.println(v1-v2);
+//        System.out.println(v1*v2);
+
+
+        //總分級平均成績
+//        System.out.println("輸入國文,英文,數學,成績");
+//        Scanner scn=new Scanner(System.in);
+//        int v1=scn.nextInt();
+//        int v2=scn.nextInt();
+//        int v3=scn.nextInt();
+//        int v4=v1+v2+v3;
+//        System.out.println(v4);
+//        System.out.println(v4/3);
+
+
+        //單位換算 身高&體重
+//        Scanner scn=new Scanner(System.in);
+//        System.out.println("輸入身高(cm)");
+//        float v1=scn.nextFloat();
+//        System.out.println("輸入體重kg");
+//        float v2=scn.nextFloat();
+//        System.out.println(v1/2.54);
+//        System.out.println(v2/0.454);
+
+
+        //三角形判定
+//        Scanner scn=new Scanner(System.in);
+//        System.out.println("輸入三角形的三邊長");
+//        int v1=scn.nextInt();
+//        int v2=scn.nextInt();
+//        int v3=scn.nextInt();
+//        if ( v1+v2>=v3 && v1+v3>=v2 && v2+v3>=v1)
+//        { System.out.println("合法三角形");}
+//        else if (v1+v2<v3 || v1+v3<v2 || v2+v3<v1)
+//        { System.out.println("無此三角形存在");}
+
+
+        //閏,平年判斷
+//        Scanner scn=new Scanner(System.in);
+//        System.out.println("輸入三個1~3000的正整數代表西元年");
+//        int v1=scn.nextInt();
+//        int v2=scn.nextInt();
+//        int v3=scn.nextInt();
+//        if ( v1 % 4==0 || v1 % 100!=0 && v1 % 400==0 && v1 >= 1 && v1 <= 3000 )
+//            System.out.println("閏年");
+//        else System.out.println("平年");
+//        if ( v2 % 4==0 || v2 % 100!=0 && v2 % 400==0 && v2 >= 1 && v2 <= 3000 )
+//            System.out.println("閏年");
+//        else  System.out.println("平年");
+//        if ( v3 % 4==0 || v3 % 100!=0 && v3 % 400==0 && v3 >= 1 && v3 <= 3000 )
+//            System.out.println("閏年");
+//        else System.out.println("平年");
+
+
+
+
+
+
+
+
+
+
+    }
+}
